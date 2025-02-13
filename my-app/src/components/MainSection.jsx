4eb8fd6d15c04824395364876cfeb9bdce344c2a
Zeey76 <abdulrasaqzainab76@gmail.com>
2025-02-13 02:17:10 +0100

    .

--- my-app/src/components/MainSection.jsx
+++ my-app/src/components/MainSection.jsx
@@ -7,9 +7,9 @@ const MainSection = () => {
   return (
     <div className="flex flex-col justify-center items-center mx-auto p-6 sm:p-12 gap-8  w-full max-w-[390px] sm:max-w-[700px] bg-[#08252B] sm:bg-[#041E23] border border-[#0E464F] rounded-[32px] sm:rounded-[40px]">
       <FormHeader />
-      {/* <StepOne/> */}
+      <StepOne/>
       {/* <StepTwo/> */}
-      <StepThree />
+      {/* <StepThree /> */}
     </div>
   );
 };
